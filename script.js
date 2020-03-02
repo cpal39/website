a6fcf873e5baad53517e31bc8456dc299433246b
Christopher Paldino <cpaldino@stevens.edu>
2020-03-01 19:21:39 -0500

    nav edits

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-$("#nav").click(function(){
-	if($('#anchor1').is(':visible')){
-		$("#nav").html("Navigation &#9651;");
-		$('#anchor1').hide();
-		$('#anchor2').hide();
-		$('#anchor3').hide();
-		$('#anchor4').hide();
-		$('#anchor5').hide();
-	}
-	else{
-		$("#nav").html("Navigation &#9661;");
-		$('#anchor1').show();
-		$('#anchor2').show();
-		$('#anchor3').show();
-		$('#anchor4').show();
-		$('#anchor5').show();
-	}
+$(document).ready(function(){
+	$("#nav").click(function(){
+		if($('#anchor1').is(':visible')){
+			$("#nav").html("Navigation &#9651;");
+			$('#anchor1').hide();
+			$('#anchor2').hide();
+			$('#anchor3').hide();
+			$('#anchor4').hide();
+			$('#anchor5').hide();
+		}
+		else{
+			$("#nav").html("Navigation &#9661;");
+			$('#anchor1').show();
+			$('#anchor2').show();
+			$('#anchor3').show();
+			$('#anchor4').show();
+			$('#anchor5').show();
+		}
+	});
+	$("a[href^='#']").each(function(){
+		$(this).click(function(e){
+			e.preventDefault();
+			let id=$(this).attr('href');
+			$("#rightCol").animate({
+				scrollTop: $('#rightCol').scrollTop() + $(id).offset().top-30
+			}, 'fast');
+			// $('#rightCol').scrollTop($('#rightCol').scrollTop() + $(id).offset().top-30);
+			// $(id)[0].scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+		});
+	});
 });
